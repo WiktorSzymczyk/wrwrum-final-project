2190c44cd4876106319069ccb7c524faaef94ce9
koziu33en <wiktor.szymczyk221@gmail.com>
2022-12-17 11:04:33 +0000

    adding branch

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello</h1>
     </div>
   );
 }
