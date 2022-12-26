5183e3c5302a610556b33a07ba5df006eedb95bb
Wiktor Szymczyk <74505952+koziu33en@users.noreply.github.com>
2022-12-26 20:24:27 +0000

    Finished refractoring Constructors and Datas/Circuits, Drivers, DriverStandings

--- src/components/Constructors/ConstructorPage.tsx
+++ src/components/Constructors/ConstructorPage.tsx
@@ -7,24 +7,23 @@ import data3 from "../Datas/2023/teams.json";
 export default function ConstructorPage() {
   return (
     <>
-
-          <div className="flex flex-col justify-center">
-          {data3.Teams.map((res) => (
-            <div key={res.id}>
-              <SingleConstructor
-                name={res.Name}
-                chief={res.TeamChief}
-                powerUnit={res.PowerUnit}
-                img={res.img}
-                d1Name={res.Drivers[0].Name + " " + res.Drivers[0].Surname}
-                d2Name={res.Drivers[1].Name + " " + res.Drivers[1].Surname}
-                chasis={res.Chasis}
-                first={res.FirstTeamEntry}
-                world={res.WorldChampionships}
-              />
-            </div>
-          ))}
-        </div>
+      <div className="flex flex-col justify-center">
+        {data3.Teams.map((res) => (
+          <div key={res.id}>
+            <SingleConstructor
+              name={res.Name}
+              chief={res.TeamChief}
+              powerUnit={res.PowerUnit}
+              img={res.img}
+              d1Name={res.Drivers[0].Name + " " + res.Drivers[0].Surname}
+              d2Name={res.Drivers[1].Name + " " + res.Drivers[1].Surname}
+              chasis={res.Chasis}
+              first={res.FirstTeamEntry}
+              world={res.WorldChampionships}
+            />
+          </div>
+        ))}
+      </div>
     </>
-  );
+  )
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 export default function Home() {
-  return <div className="bg-[#222] h-[100vh] w-[100vw] flex">
-    <img src='https://www.dhl.com/content/dam/dhl/global/delivered/images/social-share-1200x630/g168-dl-formula-1-season-2022-racing-car.jpg' alt='' className="object-none pt-10 w-[100vw] h-[30vh] justify-center"/>
+  return (
+  <div className="bg-[#222] h-[100vh] w-[100vw] flex">
+    <img src='https://www.dhl.com/content/dam/dhl/global/delivered/images/social-share-1200x630/g168-dl-formula-1-season-2022-racing-car.jpg' 
+    className="object-none pt-10 w-[100vw] h-[30vh] justify-center" alt=''/>
   </div>
+  )
 }
