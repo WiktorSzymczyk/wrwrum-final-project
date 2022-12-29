62cdcb3040efcae44af0ca4e0682753ac28207c4
AdaN6 <a_disney_da@hotmail.com>
2022-12-29 16:14:48 +0100

    created a sevice folder, a newsapi file and tested the newsapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
+import {getNews} from "./services/newsApi";
+
 
 function App() {
+
+  //api testing
+ const [newsFeed, setNewsFeed] = useState(null); 
+
+useEffect(() => {
+ getNews().then(res => setNewsFeed(res))
+ 
+  }, [])
+  console.log(newsFeed)
+
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      
     </div>
   );
 }
