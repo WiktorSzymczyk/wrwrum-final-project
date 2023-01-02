cfedecf86dc493f3e60f084fbf360d23fa1e9d62
AdaN6 <a_disney_da@hotmail.com>
2023-01-02 10:19:44 +0100

    imported logo

--- src/pages/KidsZone.tsx
+++ src/pages/KidsZone.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import  '../assets/img/logo.svg'
 
 const KidsZone = () => {
   return (
-    <div className='m-11'>
+    <div className='m-11 self-center place-self-center'>
         <div className='modalSmall'>
-             Kids zone
+            <img src="../assets/img/logo.svg" />
         </div>
     </div>
   )
