088fb0854f7c1562180362e779d53c62d52790a0
Wiktor Szymczyk <wiktor.szymczyk221@gmail.com>
2023-01-03 15:03:48 +0000

    Found a way to display the data using name as a parameter from the table

--- src/components/Drivers/DriversInformation.tsx
+++ src/components/Drivers/DriversInformation.tsx
@@ -3,12 +3,15 @@ import { useParams } from 'react-router-dom';
 import d2023 from '../../Datas/2023/drivers.json';
 
 export default function DriversInformation() {
-	const params = useParams<{ name: any }>();
+	const params = useParams<{ name: string }>();
 
+	const index = d2023.driver.findIndex((driver) => driver.id);
 	return (
 		<>
 			<div className='text-white'>{params.name}</div>
 			{/* <h3>{d2023.driver.findIndex(params.name)}</h3> */}
+			{/* <h1 className='text-white'>{index}</h1> */}
+			<h1 className='text-white'>{d2023.driver[index - 1].country}</h1>
 		</>
 	);
 }
