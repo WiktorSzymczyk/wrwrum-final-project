57b167ed7399141573aabaab8ffdd668747d61a7
AdaN6 <a_disney_da@hotmail.com>
2023-01-05 16:57:18 +0100

    created and styled  kid zone card

--- src/pages/KidsZoneIndex.tsx
+++ src/pages/KidsZoneIndex.tsx
@@ -1,7 +1,17 @@
+import "../assets/styles/kidsZone/kidsZoneIndex.scss";
+
 const KidsZoneIndex = () => {
   return (
     <div>
-      <h1>Kids Zone</h1>
+      <div className="kids-zone-container">
+        <h1 className="kids-zone-title">Kids Zone</h1>
+        <div className="modalSmall kids-zone-card">
+          <div>
+            <img src="/img/kids_zone/memory_game.jpg"></img>
+          </div>
+          <h3>Memory Game</h3>
+        </div>
+      </div>
     </div>
   )
 }
