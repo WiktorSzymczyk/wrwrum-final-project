15464375da3e7f7b8d1310379cc4761ec83933c9
AdaN6 <a_disney_da@hotmail.com>
2023-01-05 17:16:58 +0100

    added onClick function on the kid zone card

--- src/pages/KidsZoneIndex.tsx
+++ src/pages/KidsZoneIndex.tsx
@@ -1,11 +1,20 @@
 import "../assets/styles/kidsZone/kidsZoneIndex.scss";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const KidsZoneIndex = () => {
+
+  let navigate = useNavigate();
+
+  const handleClick = () => {
+   let path: string = '/kidszone/memorygame';
+   navigate(path);
+  }
+
   return (
     <div>
       <div className="kids-zone-container">
         <h1 className="kids-zone-title">Kids Zone</h1>
-        <div className="modalSmall kids-zone-card">
+        <div className="modalSmall kids-zone-card" onClick={handleClick}>
           <div>
             <img src="/img/kids_zone/memory_game.jpg"></img>
           </div>
