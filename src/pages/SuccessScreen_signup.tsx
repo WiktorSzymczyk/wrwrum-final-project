a5cd9d82d01ab89a0e27b3f755d8db574cd0a4f3
AdaN6 <a_disney_da@hotmail.com>
2023-01-17 16:10:44 +0100

    updated successscreen

--- src/pages/SuccessScreen_signup.tsx
+++ src/pages/SuccessScreen_signup.tsx
@@ -6,7 +6,7 @@ const SuccessScreen_signup = () => {
         <div>
             <div className='modalMedium'>
                 <div className="signup-body">
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+                    {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> */}
                     <h1 className="mb-11  font-bold"> Congratulation!</h1>
 
                 </div>
