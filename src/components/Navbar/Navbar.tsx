198ab737f9969cf7dc7f156a624e9b0f4442ef07
AdaN6 <a_disney_da@hotmail.com>
2023-01-18 03:30:09 +0100

    added navbar bg colour to colour library

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,13 +2,8 @@ import React, { useState } from 'react';
 import Logo from './Logo';
 import NavbarLists from './NavbarLists';
 
-
 export default function Navbar() {
 	const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
-
-	
-
-
 
 
 	return (
